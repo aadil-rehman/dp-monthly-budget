a1da767dde9d6cfb382a43b1f51f9f30a4061b95
Moh Aadil <aadilrehman.development@gmail.com>
2025-05-26 23:08:43 +0530

    set up app

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,12 @@
-console.log("hello");
+const express = require("express");
+
+const app = express();
+const PORT = 5000;
+
+app.use("/", (req, res) => {
+	res.send("Hello world");
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening on Port ${PORT}....`);
+});
